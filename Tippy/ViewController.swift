8d3a02fc3663f8d78ce68ab163c6cb9ab2fe2d89
Allan <lordsofjupiters@gmail.com>
2019-12-25 15:25:00 -0800

    Main storyboard commit

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tipValue=0.1
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipPercentLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tipSlider(_ sender: UISlider) {
+        //tipPercentLabel.text=String(format: "%.0f%%" ,sender.value * 100)
+        tipValue=Double(round(sender.value)/100)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill=Double(billField.text!) ?? 0
+        let tipPercentages=[0.12,0.15,0.18,tipValue]
+        let tipButtons=tipPercentages[tipControl.selectedSegmentIndex]
+        let tip=bill * tipButtons
+        let total = bill+tip
 
+        tipPercentLabel.text=String(format: "%.0f%%", tipButtons * 100)
+        tipLabel.text=String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
